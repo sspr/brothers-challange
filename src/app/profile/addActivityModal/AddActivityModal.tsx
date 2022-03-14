b8c95a07cd5427591ba0140c4cb51545ea019389
sspr <marcinszuscik11@gmail.com>
2022-03-14 13:19:58 +0100

    From submission fix

--- src/app/profile/addActivityModal/AddActivityModal.tsx
+++ src/app/profile/addActivityModal/AddActivityModal.tsx
@@ -1,5 +1,4 @@
 import { Box, Modal, Typography } from '@mui/material';
-import { useEffect, useState } from 'react';
 import { focusManager } from 'react-query';
 
 import { useLocale, useSnackbar } from 'hooks';
@@ -8,18 +7,14 @@ import { AddActivityModalProps } from './AddActivityModal.types';
 import { AddActivityFormContainer } from './addActivityForm/AddActivityFormContainer';
 
 export const AddActivityModal = ({ isOpened, onModalClose, name }: AddActivityModalProps) => {
-  const [shouldCloseModal, setShouldCloseModal] = useState(false);
   const { formatMessage } = useLocale();
   const { showSnackbar } = useSnackbar();
 
-  useEffect(() => {
-    if (shouldCloseModal) {
-      showSnackbar(formatMessage({ id: 'addActivity.success' }));
-      onModalClose();
-      setShouldCloseModal(false);
-      focusManager.setFocused(true);
-    }
-  }, [shouldCloseModal]);
+  const onSubmit = () => {
+    showSnackbar(formatMessage({ id: 'addActivity.success' }));
+    onModalClose();
+    focusManager.setFocused(true);
+  };
 
   return (
     <Modal open={isOpened} onClose={onModalClose} aria-labelledby="modal-add-activity">
@@ -27,7 +22,7 @@ export const AddActivityModal = ({ isOpened, onModalClose, name }: AddActivityMo
         <Typography variant="h6" sx={styles.header}>
           {formatMessage({ id: 'profile.addActivity' })}
         </Typography>
-        <AddActivityFormContainer name={name} onFormSubmission={() => setShouldCloseModal((prevState) => !prevState)} />
+        <AddActivityFormContainer name={name} onFormSubmission={onSubmit} />
       </Box>
     </Modal>
   );
